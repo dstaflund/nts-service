a03ff79a080d77657110ce099bc141f489116ac2
Darryl Staflund <darryl.staflund@hushmail.com>
2018-12-26 09:50:55 -0700

    Add '@agm/core' to project
    Note that I've deliberately left app.module.ts out of this commit
    until I secure the Google Maps API key

--- npm-client-web/src/app/app.component.ts
+++ npm-client-web/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'nts-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'npm-client-web';
+  title: string = 'My first AGM project';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
 }
