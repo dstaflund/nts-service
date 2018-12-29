0d6276c62a15e7675c86457de98d0c618070820f
Darryl Staflund <darryl.staflund@hushmail.com>
2018-12-29 12:24:30 -0700

    Add logging and streamline queries

--- npm-client-web/src/app/app.module.ts
+++ npm-client-web/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {AgmCoreModule} from '@agm/core';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyC1iO-vHZ4AdR3oriq_0OXkOv2NDzk7jQU'
+    }),
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
