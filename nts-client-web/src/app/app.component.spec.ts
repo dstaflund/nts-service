305c45602b144a8b27f3d7b93cc58f86d6398c89
Darryl Staflund <darryl.staflund@hushmail.com>
2018-12-29 12:44:00 -0700

    Rename 'npm-client-web' to 'nts-client-web'

--- nts-client-web/src/app/app.component.spec.ts
+++ nts-client-web/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'npm-client-web'`, () => {
+  it(`should have as title 'nts-client-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('npm-client-web');
+    expect(app.title).toEqual('nts-client-web');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to npm-client-web!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to nts-client-web!');
   });
 });
