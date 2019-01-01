02379cefdc8e54520fb0b96bd3b3d795162e803c
Darryl Staflund <darryl.staflund@hushmail.com>
2018-12-31 17:35:05 -0700

    Handle BigDecimal to Float conversion when matching on lat/lngs

--- nts-service/src/main/java/com/github/dstaflund/nts/match/latitude/MatchingLatitudesProvider.java
+++ nts-service/src/main/java/com/github/dstaflund/nts/match/latitude/MatchingLatitudesProvider.java
@@ -3,8 +3,10 @@ package com.github.dstaflund.nts.match.latitude;
 import com.github.dstaflund.nts.QueryExecuter;
 import org.hibernate.Session;
 
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.github.dstaflund.nts.NtsMap.MatchingLatitudesContract.QUERY_NAME;
 
@@ -16,10 +18,15 @@ final class MatchingLatitudesProvider {
     @SuppressWarnings("Duplicates")
     static List<Float> findMatchingLatitudes(MatchingLatitudesParams ctx) {
         if (ctx == null || ctx.getLatitude() == null || ctx.getLatitude().isEmpty()) return Collections.emptyList();
-        return QueryExecuter.executeQuery((Session session) ->
+        List<BigDecimal> results = QueryExecuter.executeQuery((Session session) ->
             session.getNamedNativeQuery(QUERY_NAME)
                 .setParameter(1, ctx.getLatitude().trim().replace("+", "") + "%")
                 .setParameter(2, ctx.getLatitude().trim().replace("+", "") + "%")
         );
+        return results == null
+            ? Collections.emptyList()
+            : results.parallelStream()
+                .map(BigDecimal::floatValue)
+                .collect(Collectors.toList());
     }
 }
--- nts-service/src/main/java/com/github/dstaflund/nts/match/longitude/MatchingLongitudesProvider.java
+++ nts-service/src/main/java/com/github/dstaflund/nts/match/longitude/MatchingLongitudesProvider.java
@@ -3,8 +3,10 @@ package com.github.dstaflund.nts.match.longitude;
 import com.github.dstaflund.nts.QueryExecuter;
 import org.hibernate.Session;
 
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.github.dstaflund.nts.NtsMap.MatchingLongitudesContract.QUERY_NAME;
 
@@ -16,10 +18,15 @@ final class MatchingLongitudesProvider {
     @SuppressWarnings("Duplicates")
     static List<Float> findMatchingLongitudes(MatchingLongitudesParams ctx) {
         if (ctx == null || ctx.getLongitude() == null || ctx.getLongitude().isEmpty()) return Collections.emptyList();
-        return QueryExecuter.executeQuery((Session session) ->
+        List<BigDecimal> results = QueryExecuter.executeQuery((Session session) ->
             session.getNamedNativeQuery(QUERY_NAME)
                 .setParameter(1, ctx.getLongitude().trim().replace("+", "") + "%")
                 .setParameter(2, ctx.getLongitude().trim().replace("+", "") + "%")
         );
+        return results == null
+            ? Collections.emptyList()
+            : results.parallelStream()
+            .map(BigDecimal::floatValue)
+            .collect(Collectors.toList());
     }
 }
